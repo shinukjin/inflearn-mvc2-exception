cbb11fb59ed9d522680dabfa2a4b7cd7334a9b5f
jinjin <swj0507@gmail.com>
2024-01-17 11:20:51 +0900

    inflearn-mvc2-exception - api exception

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,29 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/member/{id}")
+    public MemberDto getMemberDto(@PathVariable("id")String id) {
+        if(id.equals("ex")){
+            throw new RuntimeException("잘못된 사용자");
+        }
+        return new MemberDto(id, "heelo " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String memberId;
+        private String name;
+    }
+}
